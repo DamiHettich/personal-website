3d63b9eb8cc7be157cc353c9354f8c74ed4d8e14
El Dami <damianhettich@gmail.com>
2025-02-28 00:43:26 -0300

    animated changing definition of my own title

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Greeting.css'
 import AnimatedBlobs from './AnimatedBlobs'
 
@@ -7,6 +7,52 @@ interface GreetingProps {
 }
 
 const Greeting: React.FC<GreetingProps> = ({ scrollToAbout }) => {
+  const [displayText, setDisplayText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [loopNum, setLoopNum] = useState(0);
+  
+  const titles = [
+    'Software Engineer',
+    'Data Engineer',
+    'MLOps Specialist',
+    'Software Enthusiast',
+    'Team Leader',
+    'Consultant'
+  ];
+  
+  useEffect(() => {
+    const typingSpeed = isDeleting ? 50 : 100;
+    const currentTitle = titles[loopNum % titles.length];
+    
+    const timer = setTimeout(() => {
+      if (!isDeleting) {
+        // Typing
+        setDisplayText(currentTitle.substring(0, currentIndex + 1));
+        setCurrentIndex(prevIndex => prevIndex + 1);
+        
+        // If we've typed the full word
+        if (currentIndex >= currentTitle.length - 1) {
+          // Wait a bit before starting to delete
+          setTimeout(() => setIsDeleting(true), 1500);
+        }
+      } else {
+        // Deleting
+        setDisplayText(currentTitle.substring(0, currentIndex - 1));
+        setCurrentIndex(prevIndex => prevIndex - 1);
+        
+        // If we've deleted the full word
+        if (currentIndex <= 1) {
+          setIsDeleting(false);
+          setLoopNum(loopNum + 1);
+          setCurrentIndex(0);
+        }
+      }
+    }, typingSpeed);
+    
+    return () => clearTimeout(timer);
+  }, [currentIndex, isDeleting, loopNum, titles]);
+  
   return (
     <div className="greeting-container">
       <AnimatedBlobs />
@@ -16,9 +62,13 @@ const Greeting: React.FC<GreetingProps> = ({ scrollToAbout }) => {
           <br />
           I'm <span className="greeting-name">Dami</span>
         </h1>
-        <h2 className="greeting-subtitle">A self taught Software Engineer, always learning and building all sorts of software</h2>
+        <h2 className="greeting-subtitle">
+          A self taught <span className="typing-text">{displayText}</span>
+          <span className="cursor">|</span>
+        </h2>
         <p className="greeting-description">
-          Would you like to know more about me?
+          I'd like to show you some of my work... 
+          <br/>Care to take a look?
         </p>
         <div className="greeting-buttons">
           <button className="secondary-button" onClick={scrollToAbout}>
